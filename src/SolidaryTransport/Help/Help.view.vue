e3cc24254a4c68540e70eec68ed6ca7b3f5b6317
Alban Allée <a.allee@orchard-id.com>
2020-02-24 10:53:58 +0100

    Style Help elements and animate transition on user interactions

--- src/SolidaryTransport/Help/Help.view.vue
+++ src/SolidaryTransport/Help/Help.view.vue
@@ -5,35 +5,88 @@
             <ion-buttons slot="start">
               <ion-back-button text=""></ion-back-button>
             </ion-buttons>
-            <ion-title>{{ $t('SolidaryTransport.Help.title') }}</ion-title>
+            <ion-title>{{$t('SolidaryTransport.Help.title')}}</ion-title>
           </ion-toolbar>
         </ion-header>
 
-        <ion-content color="background" v-if="sections.length !== 0">
-          <ion-card v-for="(section, index) in sections" @click="interactWithCard(index)" :key="index" class="mc-card">
-            <ion-card-header class="mc-card">
-              <ion-card-title>{{section.title}}</ion-card-title>
-            </ion-card-header>
+        <ion-content color="background">
+          <div class="mc-rounded-cards" v-if="sections.length !== 0">
+            <ion-card v-for="(section, index) in sections" @click="interactWithCard(index)" :key="index" class="mc-card">
+              <ion-card-header class="mc-card-header">
+                <ion-card-title class="mc-card-title">{{section.title}}</ion-card-title>
+              </ion-card-header>
 
-            <ion-card-content class="mc-card-content" v-if="active === index">
-              <p v-for="(paragraph, index) in section.paragraphs" v-html="paragraph.text"></p>
-            </ion-card-content>
-          </ion-card>
-        </ion-content>
+              <ion-card-content class="mc-card-content">
+                <div class="mc-card-content-wrapper" :class="{'is-active': active === index}">
+                  <p v-for="(paragraph, index) in section.paragraphs" v-html="paragraph.text"></p>
+                </div>
+              </ion-card-content>
+            </ion-card>
+          </div>
 
-        <ion-content color="background" v-else>
-          loading...
+          <div class="mc-rounded-cards" v-else>
+            <div class="mc-loading">
+              <ion-spinner name="crescent"></ion-spinner>
+              <span class="text">{{$t('SolidaryTransport.Help.loading')}}</span>
+            </div>
+          </div>
         </ion-content>
     </ion-page>
 </template>
 
 <style lang="scss">
-  .mc-card {
+  .mc-rounded-cards {
+    border-radius: 15px 15px 0px 0px;
+    background-color: var(--ion-color-primary);
+    padding: 30px;
+    min-height: 100%;
 
-    .mc-card-content {
-      p + p {
-        margin-top: 10px;
+    .mc-card {
+      background-color: #F5F6FA;
+      margin-left: 0;
+      margin-right: 0;
+      box-shadow: none;
+      border-radius: 15px;
+
+      + .mc-card {
+        margin-top: 20px;
       }
+
+      .mc-card-header {
+        padding: 13px 20px;
+
+        .mc-card-title {
+          color: var(--ion-color-background);
+          font-weight: bold;
+          font-size: 16px;
+        }
+      }
+
+      .mc-card-content {
+        font-size: 14px;
+        padding: 0;
+
+        .mc-card-content-wrapper {
+          padding: 0 20px;
+          max-height: 0;
+          opacity: 0;
+          transition: max-height .7s ease, opacity .5s ease, padding .1s ease;
+
+          &.is-active {
+            padding: 0 20px 15px;
+            opacity: 1;
+            max-height: 600px;
+          }
+        }
+
+        p + p {
+          margin-top: 10px;
+        }
+      }
+    }
+
+    .mc-loading {
+
     }
   }
 </style>
