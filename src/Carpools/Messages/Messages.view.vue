f3c647026348b968b986b63967f81f86c3e1ce63
volteris <damien.blaise@smartfizz.fr>
2020-02-28 14:19:38 +0100

    Ajout de la vue mes messages (covoiturage et direct)

--- src/Carpools/Messages/Messages.view.vue
+++ src/Carpools/Messages/Messages.view.vue
@@ -11,24 +11,48 @@
 
     <ion-content color="background" no-bounce>
       <div class="mc-white-container mc-white-container-no-padding">
-        <ion-list >
+
+        <div class="d-flex justify-between top-buttons-padding">
+          <ion-button class='mc-small-button' color="primary" :fill="direct ? 'outline' : 'solid'" @click="direct = false">
+            <ion-icon name="car"></ion-icon> <span class="ion-margin-start">{{ $t('Messages.carpool') }}</span>
+          </ion-button>
+          <ion-button class='mc-small-button' color="primary" :fill="direct ? 'solid' : 'outline'" @click="direct = true">
+            <ion-icon name="chatbubbles"></ion-icon> <span class="ion-margin-start">{{ $t('Messages.direct') }}</span>
+          </ion-button>
+        </div>
+        <hr style="margin-bottom: 0px">
+
+
+        <div class="ion-text-center ion-margin-top" v-if="direct ?  $store.state.messageStore.statusMessagesDirect == 'loading' : $store.state.messageStore.statusMessagesCarpool == 'loading'">
+          <ion-icon size="large" color="background" class="rotating"  name="md-sync"></ion-icon>
+        </div>
+
+        <div class="ion-text-center ion-margin-top" v-if="direct ?  $store.state.messageStore.statusMessagesDirect == 'error' : $store.state.messageStore.statusMessagesCarpool == 'error'">
+          {{ $t('Messages.error')}}
+        </div>
+
+        <div class="ion-text-center ion-margin-top" v-if="(direct ?  $store.state.messageStore.statusMessagesDirect == 'success' : $store.state.messageStore.statusMessagesCarpool == 'success') && messages.length == 0">
+          {{ $t('Messages.none')}}
+        </div>
+
+        <ion-list v-if="direct ?  $store.state.messageStore.statusMessagesDirect == 'success' : $store.state.messageStore.statusMessagesCarpool == 'success'">
           <ion-item-sliding v-for="message in messages" :key="message.id">
             <ion-item class="message-list">
-              <ion-avatar class="ion-margin-end ion-margin-start">
+              <ion-avatar class="ion-margin-end ion-margin-start avatar">
                 <img v-if="message.avatarsRecipient" v-bind:src="message.avatarsRecipient" alt=""/>
                 <img v-if="!message.avatarsRecipient" src="/assets/user.png" alt="" />
               </ion-avatar>
               <ion-label>
-                <p>{{ message.id }}</p>
-                <p><b>{{message.givenName}} {{message.shortFamilyName}}</b></p>
+                <p class="message-name"><b>{{message.givenName}} {{message.shortFamilyName}}</b></p>
                 <p v-if="message.carpoolInfos">{{message.carpoolInfos.origin}} → {{message.carpoolInfos.destination}}</p>
-                <p>{{message.date}}</p>
-
+                <p>{{message.date | moment('utc', 'ddd D MMMM à HH[h]mm')}}</p>
               </ion-label>
             </ion-item>
+            <!--
             <ion-item-options side="end">
-              <ion-item-option @click="deleteMessage(message.idMessage)">{{$t('Commons.delete')}}</ion-item-option>
+              <ion-item-option color="danger" @click="deleteThreadAlertConfirm(message.idMessage)">{{$t('Commons.delete')}}</ion-item-option>
             </ion-item-options>
+            -->
           </ion-item-sliding>
         </ion-list>
       </div>
@@ -37,9 +61,22 @@
 </template>
 
 <style lang="scss">
+  .top-buttons-padding{
+    padding-right: 30px;
+    padding-left: 30px;
+  }
  .message-list {
    color: var(--ion-color-primary)
  }
+  .message-name{
+    font-size: 16px !important
+  }
+
+  .avatar {
+    width: 60px;
+    height: 60px;
+    background-color: #c4c4c4;
+  }
 </style>
 
 <script>
@@ -47,20 +84,51 @@
     name: 'messages',
     data () {
       return {
+        direct: false
       }
     },
       computed: {
           messages : {
               get() {
-                  return this.$store.state.messageStore.messages
+                console.log(this.direct);
+                  return this.direct ? this.$store.state.messageStore.messagesDirect : this.$store.state.messageStore.messagesCarpool
               },
           }
 
       },
       created() {
-          this.$store.dispatch('getAllMessagesCarpool', this.$store.state.userStore.user.id )
+          this.$store.dispatch('getAllMessagesCarpool', this.$store.state.userStore.user.id );
+          this.$store.dispatch('getAllMessagesDirect', this.$store.state.userStore.user.id );
       },
     methods: {
+      deleteThreadAlertConfirm(messageId) {
+        return this.$ionic.alertController
+          .create({
+            header: this.$t("Messages.confirmDeleteHeader"),
+            message: this.$t("Messages.confirmDeleteText"),
+            buttons: [
+              {
+                text: this.$t("Commons.cancel"),
+                role: 'cancel',
+                cssClass: 'secondary'
+              },
+              {
+                text: this.$t("Commons.confirm"),
+                handler: () => {
+                  this.deleteThread(messageId);
+                },
+              },
+            ],
+          })
+          .then(a => a.present())
+      },
+      deleteThread(messageId) {
+        this.$store.dispatch('deleteThread', messageId).then(res => {
+          this.presentToast(this.$t("Message.delete-success"), "success");
+        }).catch(err => {
+          this.presentToast(this.$t("Commons.error"), "tertiary");
+        })
+      },
     }
   }
 </script>
--- src/Shared/Store/message.store.js
+++ src/Shared/Store/message.store.js
@@ -2,22 +2,36 @@ import http from '../Mixin/http.mixin'
 
 export const messageStore = {
   state: {
-    statusMessage: '',
-    messages: []
+    statusMessagesCarpool: '',
+    messagesCarpool: [],
+    statusMessagesDirect: '',
+    messagesDirect: []
   },
   mutations: {
-    message_request(state) {
-      state.statusMessage = 'loading';
+    message_carpool_request(state) {
+      state.statusMessagesCarpool = 'loading';
     },
 
-    message_success(state, messages) {
-      state.statusMessage = 'success';
-      state.messages = messages;
+    message_carpool_success(state, messages) {
+      state.statusMessagesCarpool = 'success';
+      state.messagesCarpool = messages;
     },
 
-    message_error(state) {
-      state.statusMessage = 'error';
-      state.messages = [];
+    message_carpool_error(state) {
+      state.statusMessagesCarpool = 'error';
+    },
+
+    message_direct_request(state) {
+      state.statusMessagesDirect = 'loading';
+    },
+
+    message_direct_success(state, messages) {
+      state.statusMessagesDirect = 'success';
+      state.messagesDirect = messages;
+    },
+
+    message_direct_error(state) {
+      state.statusMessagesDirect = 'error';
     },
 
   },
@@ -28,20 +42,51 @@ export const messageStore = {
      */
     getAllMessagesCarpool({commit}, userId) {
       // return http.post("/carpools", data)
-      commit('message_request');
+      commit('message_carpool_request');
       return new Promise((resolve, reject) => {
 
         http.get("/users/" + userId + "/threadsCarpoolMessages").then(resp => {
           if (resp) {
-            commit('message_success', resp.data.threads);
+            commit('message_carpool_success', resp.data.threads);
             resolve(resp)
           }
         }).catch(err => {
-          commit('message_error');
+          commit('message_carpool_error');
           reject(err)
         })
       })
     },
+    getAllMessagesDirect({commit}, userId) {
+      // return http.post("/carpools", data)
+      commit('message_direct_request');
+      return new Promise((resolve, reject) => {
+
+        http.get("/users/" + userId + "/threadsDirectMessages").then(resp => {
+          if (resp) {
+            commit('message_direct_success', resp.data.threads);
+            resolve(resp)
+          }
+        }).catch(err => {
+          commit('message_direct_error');
+          reject(err)
+        })
+      })
+    },
+    deleteThread({commit, dispatch}, messageId) {
+      return new Promise((resolve, reject) => {
+
+        http.delete("/messages/" + messageId).then(resp => {
+          if (resp) {
+            dispatch('getAllMessagesCarpool');
+            dispatch('getAllMessagesDirect');
+            resolve(resp)
+          }
+        }).catch(err => {
+          reject(err)
+        })
+      })
+    },
+
   },
 
 
