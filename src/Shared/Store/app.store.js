74a0613391b52dad7848d4d8fdbfb557ee21281e
arimet <anthony.rimet@jukeback.com>
2020-03-09 15:18:35 +0100

    :sparkles: Logout anonymous systeme + loader slider

--- src/Shared/Store/app.store.js
+++ src/Shared/Store/app.store.js
@@ -1,6 +1,7 @@
 export const appStore = {
   state: {
     redirectionUrl: '',
+    seeSliderLoader: false
   },
   mutations: {
     redirectionUrl_reset(state) {
@@ -10,11 +11,23 @@ export const appStore = {
     redirectionUrl_change(state, redirectionUrl) {
       state.redirectionUrl = redirectionUrl;
     },
+
+    init_sliderloader_visibility(state) {
+      state.seeSliderLoader = false;
+    },
+
+    change_sliderloader_visibility(state) {
+      state.seeSliderLoader = !state.seeSliderLoader;
+    },
   },
   actions: {},
   getters: {
     redirectionUrl: state => {
       return state.redirectionUrl
+    },
+
+    getSliderLoader: state => {
+      return state.seeSliderLoader;
     }
   }
 }
