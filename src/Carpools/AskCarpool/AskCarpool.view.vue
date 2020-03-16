58b74420d8098398a28a465436f65eebd10e522e
arimet <anthony.rimet@jukeback.com>
2020-03-16 17:14:53 +0100

    :construction: Ask carpool step one

--- src/Carpools/AskCarpool/AskCarpool.view.vue
+++ src/Carpools/AskCarpool/AskCarpool.view.vue
@@ -13,16 +13,43 @@
       <div class="mc-white-container">
         <div class="mc-carpool-ask-recap">
           <RecapCarpool v-if="carpoolRecap" ref="recap" :recap="carpoolRecap" :type="'askCarpool'"></RecapCarpool>
-          <div class="mc-carpool-ask-firstQuestion">
-            <p>
-              <b>Je souhaite covoiturer pour...</b>
-            </p>
-            <p>Sélectionnez le ou les trajets souhaités</p>
+          <div class="mc-carpool-ask-firstStep">
+            <div class="mc-carpool-ask-question">
+              <p>
+                <b>Je souhaite covoiturer pour...</b>
+              </p>
+              <p>Sélectionnez le ou les trajets souhaités</p>
+            </div>
+            <div class="d-flex mc-ask-carpool-buttons">
+              <div
+                class="mc-button-step flex-1"
+                v-bind:class="{ 'button-step-selected': outwardSelected == true }"
+                color="primary"
+                expand="block"
+                v-on:click="selectOutward()"
+              >Aller</div>
 
-            <div class="d-flex">
-              <ion-button class="mc-big-button flex-1" color="primary" expand="block" fill="outline">Aller</ion-button>
-
-              <ion-button class="mc-big-button flex-1" color="primary" expand="block" fill="outline">Retour</ion-button>
+              <div
+                class="mc-button-step flex-1"
+                v-bind:class="{ 'button-step-selected': returnSelected == true }"
+                color="primary"
+                expand="block"
+                v-on:click="selectReturn()"
+              >Retour</div>
+            </div>
+            <div class="mc-ask-select-outward" v-if="outwardSelected">
+              <div class="mc-ask-header">L'aller</div>
+              <div class="mc-ask-outward-choose-day">
+                <div v-for="(item, index) in getOutwardDay()" v-bind:key="index">
+                  <ion-item>
+                    <ion-label>{{ $t(`${item.trad}`) }}: {{item.time}} </ion-label>
+                    <ion-checkbox
+                      slot="end"
+                      :disabled="!item.value"
+                    ></ion-checkbox>
+                  </ion-item>
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -35,11 +62,44 @@
 .mc-carpool-ask-recap {
   background: rgba(var(--ion-color-primary-rgb), 0.1);
   border-radius: 20px;
-  .mc-carpool-ask-firstQuestion {
-    padding: 20px;
+  .mc-carpool-ask-firstStep {
     color: var(--ion-color-primary);
+
+    .mc-carpool-ask-question,
+    .mc-ask-carpool-buttons {
+      padding: 10px 20px;
+    }
     p {
       margin: 0;
+    }
+
+    .mc-ask-outward-choose-day{
+      background: rgba(var(--ion-color-primary-rgb), 0.1);
+      padding: 20px;
+    }
+
+    .mc-button-step {
+      text-align: center;
+      justify-content: center;
+      align-items: center;
+      display: flex;
+      border: 2px solid;
+      border-radius: 40px;
+      padding: 10px;
+      cursor: pointer;
+      font-weight: bold;
+    }
+
+    .button-step-selected {
+      background: var(--ion-color-success);
+      color: white;
+    }
+
+    .mc-ask-header {
+      background: #f5f6fa;
+      text-align: center;
+      font-weight: bold;
+      padding: 10px;
     }
   }
 }
@@ -59,7 +119,9 @@ export default {
       carpoolSelected: null,
       carpoolRecap: null,
       hasOutward: false,
-      hasReturn: false
+      hasReturn: false,
+      outwardSelected: false,
+      returnSelected: false
     };
   },
   created() {
@@ -71,7 +133,6 @@ export default {
     getCarpoolAsk() {
       const index = this.$route.params.id;
       this.carpoolSelected = this.$store.getters.resultSearch[index];
-      console.log(this.carpoolSelected);
       this.carpoolRecap = new RecapCarpoolDTO().fromAskCarpool(
         this.carpoolSelected
       );
@@ -80,11 +141,97 @@ export default {
     },
 
     checkDaySlot() {
-      let result = carpool.resultDriver;
-      if (!!!result) result = carpool.resultPassenger;
-
+      const result = this.getResultDriveOrPassenger();
       this.hasOutward = !!result.outward;
       this.hasReturn = !!result.return;
+    },
+
+    selectOutward() {
+      this.outwardSelected = !this.outwardSelected;
+    },
+
+    selectReturn() {
+      this.returnSelected = !this.returnSelected;
+    },
+
+    getResultDriveOrPassenger() {
+      let result = this.carpoolSelected.resultDriver;
+      if (!!!result) result = this.carpoolSelected.resultPassenger;
+
+      return result;
+    },
+
+    getOutwardDay() {
+      const resultDriverOrPassenger = this.getResultDriveOrPassenger();
+      console.log(resultDriverOrPassenger.outward.wedCheck);
+      if (!!resultDriverOrPassenger.outward) {
+        const result = [];
+        result.push({
+          trad: "Carpool.L",
+          value: !!resultDriverOrPassenger.outward.monCheck,
+          time:
+            !!resultDriverOrPassenger.outward.monTime &&
+            this.$moment(resultDriverOrPassenger.outward.monTime).format(
+              "HH:mm"
+            )
+        });
+        result.push({
+          trad: "Carpool.Ma",
+          value: !!resultDriverOrPassenger.outward.tueCheck,
+          time:
+            !!resultDriverOrPassenger.outward.tueTime &&
+            this.$moment(resultDriverOrPassenger.outward.tueTime).format(
+              "HH:mm"
+            )
+        });
+        result.push({
+          trad: "Carpool.Me",
+          value: !!resultDriverOrPassenger.outward.wedCheck,
+          time:
+            !!resultDriverOrPassenger.outward.wedTime &&
+            this.$moment(resultDriverOrPassenger.outward.wedTime).format(
+              "HH:mm"
+            )
+        });
+        result.push({
+          trad: "Carpool.J",
+          value: !!resultDriverOrPassenger.outward.thuCheck,
+          time:
+            !!resultDriverOrPassenger.outward.thuTime &&
+            this.$moment(resultDriverOrPassenger.outward.thuTime).format(
+              "HH:mm"
+            )
+        });
+        result.push({
+          trad: "Carpool.V",
+          value: !!resultDriverOrPassenger.outward.friCheck,
+          time:
+            !!resultDriverOrPassenger.outward.friTime &&
+            this.$moment(resultDriverOrPassenger.outward.friTime).format(
+              "HH:mm"
+            )
+        });
+        result.push({
+          trad: "Carpool.S",
+          value: !!resultDriverOrPassenger.outward.satCheck,
+          time:
+            !!resultDriverOrPassenger.outward.satTime &&
+            this.$moment(resultDriverOrPassenger.outward.satTime).format(
+              "HH:mm"
+            )
+        });
+        result.push({
+          trad: "Carpool.D",
+          value: !!resultDriverOrPassenger.outward.sunCheck,
+          time:
+            !!resultDriverOrPassenger.outward.sunTime &&
+            this.$moment(resultDriverOrPassenger.outward.sunTime).format(
+              "HH:mm"
+            )
+        });
+
+        return result;
+      }
     }
   }
 };
--- src/Carpools/GeoSearch/GeoSearch.view.vue
+++ src/Carpools/GeoSearch/GeoSearch.view.vue
@@ -1,156 +1,175 @@
 <template>
- <div class="ion-page">
-  <ion-header no-border>
-    <ion-toolbar color="primary">
-      <ion-buttons slot="start">
-        <ion-back-button></ion-back-button>
-      </ion-buttons>
-      <h1 class="ion-text-center"> {{$t(`Search.${type}`) }} </h1>
-    </ion-toolbar>
-  </ion-header>
+  <div class="ion-page">
+    <ion-header no-border>
+      <ion-toolbar color="primary">
+        <ion-buttons slot="start">
+          <ion-back-button></ion-back-button>
+        </ion-buttons>
+        <h1 class="ion-text-center">{{$t(`Search.${type}`) }}</h1>
+      </ion-toolbar>
+    </ion-header>
 
-  <ion-content color="primary" no-bounce>
-    <div class="mc-white-container" style="height: 100%">
-      <ion-item>
-        <ion-input
-          ref="inputSearch"
-          type="text"
-          name="date"
-          v-bind:placeholder="$t('Search.placeHolder')"
-          @input="submitSearch($event)">
-        </ion-input>
-        <ion-icon size="large" color="primary" class="rotating" v-if="this.$store.state.searchStore.statusGeo == 'loading'" name="md-sync"></ion-icon>
-      </ion-item>
+    <ion-content color="primary" no-bounce>
+      <div class="mc-white-container" style="height: 100%">
+        <ion-item>
+          <ion-input
+            ref="inputSearch"
+            type="text"
+            name="date"
+            v-bind:placeholder="$t('Search.placeHolder')"
+            @input="submitSearch($event)"
+          ></ion-input>
+          <ion-icon
+            size="large"
+            color="primary"
+            class="rotating"
+            v-if="this.$store.state.searchStore.statusGeo == 'loading'"
+            name="md-sync"
+          ></ion-icon>
+        </ion-item>
 
-      <div v-for="address in this.$store.state.searchStore.resultsGeo" :key="address.key">
-        <ion-item class="flex-card" v-if="address.displayLabel[0]" v-on:click="selectGeo(address)">
-          <div class='iconSearch'>
-            <img v-bind:src="address.icon" alt="">
-          </div>
+        <div v-for="address in this.$store.state.searchStore.resultsGeo" :key="address.key">
+          <ion-item
+            class="flex-card"
+            v-if="address.displayLabel[0]"
+            v-on:click="selectGeo(address)"
+          >
+            <div class="iconSearch">
+              <img v-bind:src="address.icon" alt />
+            </div>
             <ion-card-header>
               <ion-card-title>{{ address.displayLabel[0] }}</ion-card-title>
               <ion-card-subtitle>
-                  <div>{{ address.displayLabel[1] }}</div>
+                <div>{{ address.displayLabel[1] }}</div>
               </ion-card-subtitle>
             </ion-card-header>
-        </ion-item>
+          </ion-item>
+        </div>
       </div>
-    </div>
-  </ion-content>
+    </ion-content>
   </div>
 </template>
 
 <style lang="scss">
-  .flex-card {
-    display: flex;
-    align-items: center;
+.flex-card {
+  display: flex;
+  align-items: center;
 
-    & .iconSearch {
-      width: 30px;
-      margin-left: 20px;
-    }
+  & .iconSearch {
+    width: 30px;
+    margin-left: 20px;
   }
+}
 </style>
 
 <script>
-  import { toast } from '../../Shared/Mixin/toast.mixin';
+import { toast } from "../../Shared/Mixin/toast.mixin";
 
-  export default {
-    name: 'geo-search',
-    mixins: [toast],
-    components: {
-    },
-    created () {
-      this.$store.state.searchStore.resultsGeo = [];
-       this.unwatch = this.$store.watch(
-        (state, getters) => getters.addressessUseToPost,
-        (newValue, oldValue) => {
-          this.$store.dispatch('treatementUpdateAddresses')
-        },
-      );
-    },
-    beforeDestroy() {
-      this.unwatch();
-    },
-    data () {
-      return {
-        type: this.$route.query.type,
-        action: this.$route.query.action,
-        index: this.$route.query.index
+export default {
+  name: "geo-search",
+  mixins: [toast],
+  components: {},
+  created() {
+    this.$store.state.searchStore.resultsGeo = [];
+    this.unwatch = this.$store.watch(
+      (state, getters) => getters.addressessUseToPost,
+      (newValue, oldValue) => {
+        this.$store.dispatch("treatementUpdateAddresses");
       }
-    },
+    );
+  },
+  beforeDestroy() {
+    this.unwatch();
+  },
+  data() {
+    return {
+      type: this.$route.query.type,
+      action: this.$route.query.action,
+      index: this.$route.query.index
+    };
+  },
 
-    methods: {
-
-      submitSearch: function(event) {
+  methods: {
+    submitSearch: function(event) {
+      if (this.searchTimeout) clearTimeout(this.searchTimeout);
+      this.searchTimeout = setTimeout(() => {
         const searchQuery = event.target.value;
-        if (!! searchQuery) {
-          this.$store.dispatch('geoSearch', { searchQuery }).catch(err => {
-            this.presentToast("Une erreur est survenue", 'danger')
+        if (!!searchQuery) {
+          this.$store.dispatch("geoSearch", { searchQuery }).catch(err => {
+            this.presentToast("Une erreur est survenue", "danger");
           });
         }
-      },
+      }, 500);
+    },
 
-      selectGeo: function(address) {
-        let addressDTO = address;
+    selectGeo: function(address) {
+      let addressDTO = address;
 
-        const displayGeo = `${address.addressLocality}, ${address.addressCountry}`;
+      const displayGeo = `${address.addressLocality}, ${address.addressCountry}`;
 
-        switch (this.action) {
-          case 'search': {
-
-            switch (this.type) {
-              case 'origin': {
-                this.$store.commit('changeOrigin', { addressDTO, displayGeo });
-                break;
-              }
-
-              case 'destination': {
-                this.$store.commit('changeDestination', { addressDTO, displayGeo });
-                break;
-              }
-
-              case 'register_address': {
-                this.$store.commit('changeRegisterAddress', { addressDTO, displayGeo });
-                break;
-              }
-
-              case 'update_user_address': {
-                this.$store.commit('updateUserAddress', { addressDTO, displayGeo });
-                break;
-              }
-
+      switch (this.action) {
+        case "search": {
+          switch (this.type) {
+            case "origin": {
+              this.$store.commit("changeOrigin", { addressDTO, displayGeo });
+              break;
             }
-            break;
-          }
 
-          case 'post': {
-            switch (this.type) {
-              case 'origin': {
-                this.$store.commit('addPostCarpoolOrigin', { addressDTO });
-                break;
-              }
-
-              case 'destination': {
-                this.$store.commit('addPostCarpoolDestination', { addressDTO });
-                break;
-              }
-
-              case 'step': {
-                this.$store.commit('addPostCarpoolStep', { addressDTO, index: this.index });
-                break;
-              }
-
+            case "destination": {
+              this.$store.commit("changeDestination", {
+                addressDTO,
+                displayGeo
+              });
+              break;
             }
-            break;
+
+            case "register_address": {
+              this.$store.commit("changeRegisterAddress", {
+                addressDTO,
+                displayGeo
+              });
+              break;
+            }
+
+            case "update_user_address": {
+              this.$store.commit("updateUserAddress", {
+                addressDTO,
+                displayGeo
+              });
+              break;
+            }
           }
-
-
-          default:
-            break;
+          break;
         }
-        this.$router.back();
+
+        case "post": {
+          switch (this.type) {
+            case "origin": {
+              this.$store.commit("addPostCarpoolOrigin", { addressDTO });
+              break;
+            }
+
+            case "destination": {
+              this.$store.commit("addPostCarpoolDestination", { addressDTO });
+              break;
+            }
+
+            case "step": {
+              this.$store.commit("addPostCarpoolStep", {
+                addressDTO,
+                index: this.index
+              });
+              break;
+            }
+          }
+          break;
+        }
+
+        default:
+          break;
       }
+      this.$router.back();
     }
   }
+};
 </script>
