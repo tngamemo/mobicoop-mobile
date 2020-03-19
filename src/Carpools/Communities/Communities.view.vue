cbeb0a8b0e9817bb374b6c80abe317e210c342cb
arimet <anthony.rimet@jukeback.com>
2020-03-19 16:37:32 +0100

    :sparkles: Add communities pages with search

--- src/Carpools/Communities/Communities.view.vue
+++ src/Carpools/Communities/Communities.view.vue
@@ -5,31 +5,148 @@
         <ion-buttons slot="start">
           <ion-back-button></ion-back-button>
         </ion-buttons>
-        <h1 class="ion-text-center"> {{ $t('Communities.title') }} </h1>
+        <h1 class="ion-text-center">{{ $t('Communities.title') }}</h1>
       </ion-toolbar>
     </ion-header>
 
     <ion-content color="primary" no-bounce>
-      <div class="mc-white-container">
+      <div class="mc-communities-first-block">
+        <div class="mc-my-communities" v-if="!!userCommunities">
+          <p>Mes communautés:</p>
+          <div class="d-flex">
+            <div
+              item
+              v-for="(commu, index) in userCommunities"
+              :key="index"
+              class="mc-my-community"
+            >
+              <ion-thumbnail>
+                <img :src="!!commu.images[0] && commu.images[0].versions.square_250" />
+              </ion-thumbnail>
+              {{commu.name}}
+            </div>
+          </div>
+        </div>
 
+        <div class="mc-communities-search">
+          <ion-searchbar
+            @ionInput="searchText = $event.target.value"
+            @ionClear="searchText = ''"
+            color="transparent"
+            placeholder="Rechercher une communauté"
+          ></ion-searchbar>
+        </div>
+
+        <ion-button class="mc-big-button" color="success" expand="block">
+          <ion-icon name="add" class="ion-padding-end"></ion-icon>Créer une communauté
+        </ion-button>
+      </div>
+      <div class="mc-white-container">
+        <ion-item item v-for="(commu, index) in communities" :key="index">
+          <div class="d-flex mc-communities-community">
+            <div class="mc-communities-avatar">
+              <ion-thumbnail>
+                <img :src="!!commu.images[0] && commu.images[0].versions.square_250" />
+              </ion-thumbnail>
+            </div>
+            <div class="mc-communities-text">
+              {{commu.name}}
+              <ion-button color="success" expand="block">
+                <ion-icon name="eye" class="ion-padding-end"></ion-icon>
+                {{ $t('Communities.see') }}
+              </ion-button>
+            </div>
+          </div>
+        </ion-item>
       </div>
     </ion-content>
   </div>
 </template>
 
 <style lang="scss">
+.mc-communities-first-block {
+  margin: 30px;
+  .mc-my-communities {
+    margin-bottom: 20px;
+    padding-bottom: 10px;
+    font-weight: bold;
+    border-bottom: 1px solid white;
+    .mc-my-community {
+      border: 1px solid white;
+      border-radius: 50%;
+      width: 80px;
+      height: 80px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      font-weight: bolds;
 
+      ion-thumbnail {
+        --border-radius: 50%;
+      }
+    }
+  }
+  .searchbar-input {
+    box-shadow: none !important;
+  }
+  .mc-communities-search {
+    margin-bottom: 20px;
+    ion-searchbar {
+      --box-shadow: none !important;
+      border-bottom: 1px solid white;
+    }
+  }
+}
+
+.mc-communities-community {
+  padding: 10px;
+  .mc-communities-avatar {
+    ion-thumbnail {
+      --border-radius: 50%;
+    }
+  }
+
+  .mc-communities-text {
+    margin-left: 20px;
+    color: var(--ion-color-primary);
+    font-weight: bold;
+
+    ion-button {
+      --border-radius: 30px !important;
+    }
+  }
+}
 </style>
 
 <script>
-  export default {
-    name: 'communities',
-    data () {
-      return {
+export default {
+  name: "carpool-communities",
+  data() {
+    return {
+      searchText: ""
+    };
+  },
+  created() {
+    // On récupére les communities
+    this.$store.dispatch("getAllCommunities");
 
-      }
-    },
-    methods: {
+    if (!!this.$store.getters.userId) {
+      this.$store.dispatch("getUserCommunities");
     }
-  }
+  },
+  computed: {
+    communities() {
+      return this.$store.getters.communities.filter(commu => {
+        console.log(commu.name.toUpperCase())
+        return commu.name.toUpperCase().includes(this.searchText.toUpperCase())
+      });
+    },
+
+    userCommunities() {
+      return this.$store.getters.userCommunities;
+    }
+  },
+  methods: {}
+};
 </script>
---
+++ src/Shared/Store/community.store.js
@@ -0,0 +1,78 @@
+import http from '../Mixin/http.mixin'
+
+export const communityStore = {
+  state: {
+    statusGetCommunities: '',
+    statusGetCommunity: '',
+    communities: null,
+  },
+  mutations: {
+    communities_request(state) {
+      state.statusGetCommunities = 'loading';
+    },
+
+    communities_success(state, communities) {
+      state.statusGetCommunities = 'success';
+      state.communities = communities;
+    },
+
+    communities_error(state) {
+      state.statusGetCommunities = 'error';
+    },
+
+    community_request(state) {
+      state.statusGetCommunity = 'loading';
+    },
+
+    community_success(state, community) {
+      state.statusGetCommunity = 'success';
+      state.community = community;
+    },
+
+    community_error(state) {
+      state.statusGetCommunity = 'error';
+    }
+
+  },
+  actions: {
+
+    getAllCommunities({commit}) {
+      commit('communities_request');
+      return new Promise((resolve, reject) => {
+        http.get(`/communities`)
+          .then(resp => {
+            resolve(resp)
+            commit('communities_success', resp.data['hydra:member']);
+          })
+          .catch(err => {
+            console.log('error');
+            commit('communities_error');
+            reject(err)
+          })
+      })
+    },
+
+    getSpecificCommunity({commit}) {
+      commit('community_request');
+      return new Promise((resolve, reject) => {
+        http.get(`/communities/${carpoolId}`)
+          .then(resp => {
+            resolve(resp)
+            commit('community_success');
+          })
+          .catch(err => {
+            console.log('error');
+            commit('community_error');
+            reject(err)
+          })
+      })
+    },
+  },
+  getters : {
+    communities: state => {
+      return state.communities
+    }
+  }
+}
+
+export default { communityStore };
