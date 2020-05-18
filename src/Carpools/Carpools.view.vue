dcda03dc6f86c1d7b09f8ccf297911486f68743d
volteris <damien.blaise@smartfizz.fr>
2020-05-18 17:05:51 +0200

    add configurations

--- src/Carpools/Carpools.view.vue
+++ src/Carpools/Carpools.view.vue
@@ -4,6 +4,7 @@
 
 <script>
   import {Plugins, PushNotification, PushNotificationToken, PushNotificationActionPerformed } from '@capacitor/core';
+  import {isPlatform} from "@ionic/core";
   const { PushNotifications } = Plugins;
 
 export default {
@@ -13,35 +14,36 @@ export default {
   },
   created() {
 
+    if(isPlatform(window.document.defaultView, "capacitor")) {
+      // On success, we should be able to receive notifications
+      PushNotifications.addListener('registration',
+        (token) => {
+          console.log('Push registration success, token: ' + token.value);
+          this.$store.dispatch('postPushToken', token.value)
+        }
+      );
 
-    // On success, we should be able to receive notifications
-    PushNotifications.addListener('registration',
-      (token) => {
-        console.log('Push registration success, token: ' + token.value);
-        this.$store.dispatch('postPushToken', token.value)
-      }
-    );
+      // Some issue with our setup and push will not work
+      PushNotifications.addListener('registrationError',
+        (error) => {
+          console.log('Error on registration: ' + JSON.stringify(error));
+        }
+      );
 
-    // Some issue with our setup and push will not work
-    PushNotifications.addListener('registrationError',
-      (error) => {
-        console.log('Error on registration: ' + JSON.stringify(error));
-      }
-    );
+      // Show us the notification payload if the app is open on our device
+      PushNotifications.addListener('pushNotificationReceived',
+        (notification) => {
+          console.log('Push received: ' + JSON.stringify(notification));
+        }
+      );
 
-    // Show us the notification payload if the app is open on our device
-    PushNotifications.addListener('pushNotificationReceived',
-      (notification) => {
-        console.log('Push received: ' + JSON.stringify(notification));
-      }
-    );
-
-    // Method called when tapping on a notification
-    PushNotifications.addListener('pushNotificationActionPerformed',
-      (notification) => {
-        console.log('Push action performed: ' + JSON.stringify(notification));
-      }
-    );
+      // Method called when tapping on a notification
+      PushNotifications.addListener('pushNotificationActionPerformed',
+        (notification) => {
+          console.log('Push action performed: ' + JSON.stringify(notification));
+        }
+      );
+    }
   }
 };
 </script>
